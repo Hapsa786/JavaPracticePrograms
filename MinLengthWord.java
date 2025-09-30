88483f49dd8b375213b7ab1d8e7b7691793dd66d
firoz <firozshaik.bitops@gmail.com>
2025-09-30 21:54:30 +0530

    Added Minlen Prog

---
+++ MinLengthWord.java
@@ -0,0 +1,18 @@
+public class MinLengthWord {
+    public static void main(String[] args) {
+        String[] words = {"coffee", "water", "chai", "dietcoke"};
+
+        int minIndex = 0;
+
+        for (int i = 1; i < words.length; i++) {
+            if (words[i].length() < words[minIndex].length()) {
+                minIndex = i;
+            }
+        }
+
+        // Print the result in required format
+        System.out.println("value " + words[minIndex] + " & index - " + minIndex);
+    }
+}
+
+
